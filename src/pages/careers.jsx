fe22c6afe2a70f42cc292b4cf26f96448d960775
Raja <raja.rathinam@pcisage.com>
2026-04-21 18:57:43 +0530

    ui

---
+++ src/pages/careers.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CareersScreen from "@/components/screens/careers/careers";
+import Head from "next/head";
+
+const Careers = () => {
+  return (
+    <>
+      <Head>
+        <title>Amigos | Careers</title>
+      </Head>
+      <CareersScreen />
+    </>
+  );
+};
+
+export default Careers;
